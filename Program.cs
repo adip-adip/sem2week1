d68907af2c7a67feddadecaee96ef458f3f96588
adip-adip <adipshrestha02@gmail.com>
2026-05-11 07:08:45 +0545

    Migration

--- Program.cs
+++ Program.cs
@@ -156,8 +156,11 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
-
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
 
--- obj/Debug/net8.0/sem2week1.AssemblyInfo.cs
+++ obj/Debug/net8.0/sem2week1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem2week1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5adc3a5a1300a0787424a46e3644a723becaabbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966debb2e183e39c17c608c78f7554d2a3123e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem2week1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem2week1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
